fb0b1a32cd42f86d01087e469b55fef0e05258ba
Kari <kari.lynn.dreyer@gmail.com>
2024-05-23 10:24:31 +0200

    Create GET requests

--- index.js
+++ index.js
@@ -1,2 +1,67 @@
 const express = require('express');
+morgan = require('morgan');
+
 const app = express();
+
+let topMovies = [
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+    {
+        title: '',
+        author: '',
+    },
+]
+
+//GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to Movie Nest!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', {
+        root: __dirname});
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+
+//Listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
