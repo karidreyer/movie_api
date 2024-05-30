fb2e2eede8269f45d508917d616ae1b6e0e6abd4
Kari <kari.lynn.dreyer@gmail.com>
2024-05-30 13:36:44 +0200

    add get requests for listing all movies and single movie by title

--- index.js
+++ index.js
@@ -22,103 +22,83 @@ let users = [
 
 let movies = [
     {
-        title: 'Jeanne Dielman, 23 Quai du Commerce, 1080 Bruxelles',
-        description: 'A lonely widowed housewife does her daily chores, takes care of her apartment where she lives with her teenage son, and turns the occasional trick to make ends meet. However, something happens that changes her safe routine.',
-        genre: 'Drama',
-        director: 'Chantal Akerman',
-        year: '1975',
-        imageUrl: 'https://example.com/jeanne_dielman.jpg'
+        Title: 'Vertigo',
+        Description: 'A former police detective juggles wrestling with his personal demons and becoming obsessed with a hauntingly beautiful woman.',
+        Genre: 'Mystery/Thriller',
+        Director: 'Alfred Hitchcock',
+        Year: '1958',
+        ImageUrl: 'https://example.com/vertigo.jpg'
     },
     {
-        title: 'Vertigo',
-        description: 'A former police detective juggles wrestling with his personal demons and becoming obsessed with a hauntingly beautiful woman.',
-        genre: 'Mystery/Thriller',
-        director: 'Alfred Hitchcock',
-        year: '1958',
-        imageUrl: 'https://example.com/vertigo.jpg'
+        Title: 'Citizen Kane',
+        Description: 'Following the death of publishing tycoon Charles Foster Kane, reporters scramble to uncover the meaning of his final utterance: "Rosebud."',
+        Genre: 'Drama',
+        Director: 'Orson Welles',
+        Year: '1941',
+        ImageUrl: 'https://example.com/citizen_kane.jpg'
     },
     {
-        title: 'Citizen Kane',
-        description: 'Following the death of publishing tycoon Charles Foster Kane, reporters scramble to uncover the meaning of his final utterance: "Rosebud."',
-        genre: 'Drama',
-        director: 'Orson Welles',
-        year: '1941',
-        imageUrl: 'https://example.com/citizen_kane.jpg'
+        Title: 'Tokyo Story',
+        Description: 'An old couple visit their children and grandchildren in the city; but the children have little time for them.',
+        Genre: 'Drama',
+        Director: 'Yasujirō Ozu',
+        Year: '1953',
+        ImageUrl: 'https://example.com/tokyo_story.jpg'
     },
-    {
-        title: 'Tokyo Story',
-        description: 'An old couple visit their children and grandchildren in the city; but the children have little time for them.',
-        genre: 'Drama',
-        director: 'Yasujirō Ozu',
-        year: '1953',
-        imageUrl: 'https://example.com/tokyo_story.jpg'
-    },
-    {
-        title: 'In the Mood for Love',
-        description: 'Two neighbors form a strong bond after both suspect extramarital activities of their spouses. However, they agree to keep their bond platonic so as not to commit similar wrongs.',
-        genre: 'Romance/Drama',
-        director: 'Wong Kar Wai',
-        year: '2000',
-        imageUrl: 'https://example.com/in_the_mood_for_love.jpg'
-    },
-    {
-        title: '2001: A Space Odyssey',
-        description: 'After discovering a mysterious artifact buried beneath the lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer HAL 9000.',
-        genre: 'Science Fiction',
-        director: 'Stanley Kubrick',
-        year: '1968',
-        imageUrl: 'https://example.com/2001_space_odyssey.jpg'
-    },
-    {
-        title: 'Beau travail',
-        description: 'This film focuses on a retired French Foreign Legion officer as he recalls his once glorious life leading troops in Djibouti, East Africa.',
-        genre: 'Drama',
-        director: 'Claire Denis',
-        year: '1999',
-        imageUrl: 'https://example.com/beau_travail.jpg'
-    },
-    {
-        title: 'Mulholland Dr.',
-        description: 'After a car wreck on the winding Mulholland Drive renders a woman amnesiac, she and a perky Hollywood-hopeful search for clues and answers across Los Angeles in a twisting venture beyond dreams and reality.',
-        genre: 'Mystery/Thriller',
-        director: 'David Lynch',
-        year: '2001',
-        imageUrl: 'https://example.com/mulholland_drive.jpg'
-    },
-    {
-        title: 'Man with a Movie Camera',
-        description: 'A man travels around a city with a camera slung over his shoulder, documenting urban life with dazzling invention.',
-        genre: 'Documentary',
-        director: 'Dziga Vertov',
-        year: '1929',
-        imageUrl: 'https://example.com/man_with_a_movie_camera.jpg'
-    },
-    {
-        title: 'Singin\' in the Rain',
-        description: 'A silent film production company and cast make a difficult transition to sound.',
-        genre: 'Musical/Comedy',
-        director: 'Gene Kelly and Stanley Donen',
-        year: '1952',
-        imageUrl: 'https://example.com/singin_in_the_rain.jpg'
-    }
-]
+];
 
-//Express function - automatically route requests for static files via "public" folder
+
+//GENERAL
+
+//Route requests for static files via "public" folder
 app.use(express.static('public'));
 
-//Morgan function - use Morgan middleware for logging requests
+//Log requests (using Morgan middleware)
 app.use(morgan('common'));
 
-//GET requests
+//Stand-in homepage response
 app.get('/', (req, res) => {
     res.send('Welcome to Movie Nest!');
 });
 
+
+//ENDPOINTS
+
+//Endpoint 1: Return a list of all movies to the user
 app.get('/movies', (req, res) => {
-    res.json(movies);
+    res.status(200).json(movies);
 });
 
-//Express function - error handling middleware function
+//Endpoint 2: Return data about a single movie by title to the user (description, genre, director, image URL, whether it’s featured or not)
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params; //Object Destructuring
+    const movie = movies.find( movie => movie.title === title );
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('No such movie!')
+    }
+});
+
+
+//Endpoint 3: Return data about a genre (description) by name/title (e.g., “Thriller”) 
+
+//Endpoint 4: Return data about a director (bio, birth year, death year) by name
+
+//Endpoint 5: Allow new users to register 
+
+//Endpoint 6: Allow users to update their user info (username, password, email, date of birth) 
+
+//Endpoint 7: Allow users to add a movie to their list of favourites
+
+//Endpoint 8: Allow users to remove a movie from their list of favourites 
+
+//Endpoint 9: Allow existing users to deregister 
+
+
+
+//Error Handling Function
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
