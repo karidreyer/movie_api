53faacb0a1c969d7ed0b5fb87e97e44c7f35b932
Kari <kari.lynn.dreyer@gmail.com>
2024-06-11 21:33:18 +0200

    configure passport strategies

---
+++ passport.js
@@ -0,0 +1,52 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+//"Local Strategy" to define basic HTTP authentication for login requests
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: 'Username',
+            passwordField: 'Password',
+        },
+        async (username, password, callback) => {
+            console.log(`${username} ${password}`);
+            await Users.findOne({ Username: username })
+            .then((user) => {
+                if (!user) {
+                    console.log('incorrect username');
+                    return callback(null, false, {
+                        message: 'Incorrect username or password.',
+                    });
+                }
+                console.log('finished');
+                return callback(null, user);
+            })
+            .catch((error) => {
+                if (error) {
+                    console.log(error);
+                    return callback(error);
+                }
+            })
+        }
+    )
+);
+
+//"JWT Strategy" to authenticate users based on the JWT ("bearer token") submitted alongside their request
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+    return await Users.findbyID(jwtPayload._id)
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((error) => {
+        return callback(error)
+    });
+}));
