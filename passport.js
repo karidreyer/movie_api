c6da3299127ce0dec4f130452cfa7d2cb4f0b532
Kari <kari.lynn.dreyer@gmail.com>
2024-06-11 22:30:36 +0200

    fix typo

--- passport.js
+++ passport.js
@@ -42,7 +42,7 @@ passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'your_jwt_secret'
 }, async (jwtPayload, callback) => {
-    return await Users.findByID(jwtPayload._id)
+    return await Users.findById(jwtPayload._id)
     .then((user) => {
         return callback(null, user);
     })
