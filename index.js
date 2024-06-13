748651b7e867a8ccc4273461f8580f6af47d6ead
Kari <kari.lynn.dreyer@gmail.com>
2024-06-13 15:01:54 +0200

    require CORS for privacy

--- index.js
+++ index.js
@@ -39,7 +39,22 @@ app.use(morgan('common'));
 
 
 
-//** Authorization **//
+//** Authorization and Privacy **//
+
+//Require CORS and restrict access to allowed origins
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) { //If a specific origin isn't found on the list of origins
+            let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 //Import "auth.js"
 let auth = require('./auth')(app); 
