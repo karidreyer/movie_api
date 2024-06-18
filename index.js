6113b9b977a024b2aa3bc69b5e6ca88c6a72b102
Kari <kari.lynn.dreyer@gmail.com>
2024-06-18 12:40:06 +0200

    fix online database connection code

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 // mongoose.connect('mongodb://localhost:27017/cfDB');
 
 //Online database
-mongoose.connect('process.env.CONNECTION_URI');
+mongoose.connect(process.env.CONNECTION_URI);
 
 
 
